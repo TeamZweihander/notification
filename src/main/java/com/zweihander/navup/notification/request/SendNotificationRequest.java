6445b56fc46a6ac942502a6c5a0d94b40df7469d
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2017-03-29 20:28:51 +0200

    Adds Request requirements

--- src/main/java/com/zweihander/navup/notification/request/SendNotificationRequest.java
+++ src/main/java/com/zweihander/navup/notification/request/SendNotificationRequest.java
@@ -1,9 +1,17 @@
 package com.zweihander.navup.notification.request;
 
 import com.zweihander.navup.notification.Request;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * Created by Nsovo on 2017/03/28.
  */
+@Getter
+@AllArgsConstructor
 public class SendNotificationRequest extends Request{
+
+    long userid;
+    String message;
+    String type;
 }
