c1af57d9c539c6d87d97edc93afac2f99b3dc701
Maria Qumayo <mariaqumayo38@gmail.com>
2017-03-29 11:16:33 +0200

    added SaveInCalender Response

---
+++ src/main/java/com/zweihander/navup/notification/response/SaveInCalender.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zweihander.navup.notification.response;
+
+/**
+ *
+ * @author Maria.Qumayo
+ */
+public class SaveInCalender {
+    
+    SaveInCalender(){}
+    
+    public boolean saveNotificationInCalendar(){
+        return true;
+    } 
+}
