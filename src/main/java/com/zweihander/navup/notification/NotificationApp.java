156aec1f6ddd7d3ae3da6ed20262e49d87a4fa61
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2017-04-04 16:57:37 +0200

    Run server at debug

--- src/main/java/com/zweihander/navup/notification/NotificationApp.java
+++ src/main/java/com/zweihander/navup/notification/NotificationApp.java
@@ -1,20 +1,25 @@
 package com.zweihander.navup.notification;
 
+import com.zweihander.navup.notification.config.NotificationProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
 /**
  * Created by Nsovo on 2017/03/29.
  */
 @SpringBootApplication
+@EnableConfigurationProperties({NotificationProperties.class})
+@Slf4j
 public class NotificationApp {
 
     private static SpringApplication application = new SpringApplication(NotificationApp.class);
 
     public static void main(String[] args){
         //Environment env = application.run(args).getEnvironment();
-        application.run(args);
+        application.run("--debug");
     }
 
     public static void setApplication(SpringApplication application) {
