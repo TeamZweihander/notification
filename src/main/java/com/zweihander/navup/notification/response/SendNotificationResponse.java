a1ab8a72cc3df5eeced8f29cef32b1eaabdbaf0a
georgemweshi <georgemweshi@yahoo.com>
2017-04-05 12:43:32 +0200

    Partially implemented the SendNotication Response class

--- src/main/java/com/zweihander/navup/notification/response/SendNotificationResponse.java
+++ src/main/java/com/zweihander/navup/notification/response/SendNotificationResponse.java
@@ -1,11 +1,43 @@
 package com.zweihander.navup.notification.response;
 
+import com.zweihander.navup.notification.EmailSender.Emailer;
+import com.zweihander.navup.notification.NoticeType;
 import com.zweihander.navup.notification.Response;
+import com.zweihander.navup.notification.request.SendNotificationRequest;
 
 /**
- * Created by Nsovo on 2017/03/28.
+ * Created by George on 2017/04/04.
  */
-public class SendNotificationResponse extends Response {
+public class SendNotificationResponse extends Response
+{
+
+    private NoticeType noticeType;
+
+
+    public void sendNotificationResponse(SendNotificationRequest req)
+    {
+       noticeType=req.getNtype();
+
+
+        switch (noticeType)
+        {
+            case EMAIL:
+                Emailer em=new Emailer();
+                em.sendEMail(req.getUser().getEmail(),"Confirmation Email",req.getMsg());
+
+                break;
+
+            case SMS:
+                System.out.println("Not yet Implemented");
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
 
 
 }
