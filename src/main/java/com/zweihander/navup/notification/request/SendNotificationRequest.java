8fb7b9545437392306fa3e741a7256053f202314
georgemweshi <georgemweshi@yahoo.com>
2017-04-05 12:43:00 +0200

    Partially implemented the SendNotication Request

--- src/main/java/com/zweihander/navup/notification/request/SendNotificationRequest.java
+++ src/main/java/com/zweihander/navup/notification/request/SendNotificationRequest.java
@@ -1,9 +1,40 @@
 package com.zweihander.navup.notification.request;
 
+import com.zweihander.navup.notification.NoticeType;
 import com.zweihander.navup.notification.Request;
+import com.zweihander.navup.notification.UserMock;
 
 /**
- * Created by Nsovo on 2017/03/28.
+ * Created by George on 2017/04/04.
  */
-public class SendNotificationRequest extends Request{
+
+public class SendNotificationRequest extends Request
+{
+   private NoticeType ntype;
+   private UserMock user;
+   private String msg;
+
+   public  SendNotificationRequest(NoticeType type,UserMock usr, String msg )
+    {
+      this.user=usr;
+      this.ntype=type;
+      this.msg=msg;
+
+    }
+
+
+    public NoticeType getNtype() {
+        return ntype;
+    }
+
+    public UserMock getUser() {
+        return user;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+
+
 }
