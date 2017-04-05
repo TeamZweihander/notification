3636180a159d47c1450f541c670dd8d99734d9e1
georgemweshi <georgemweshi@yahoo.com>
2017-04-05 13:02:33 +0200

    added an email not sent exception

---
+++ src/main/java/com/zweihander/navup/notification/exception/EmailNotSentException.java
@@ -0,0 +1,29 @@
+package com.zweihander.navup.notification.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(value= HttpStatus.PRECONDITION_FAILED)
+public class EmailNotSentException extends Exception {
+
+       public EmailNotSentException() {
+           super();
+    }
+
+    public EmailNotSentException(String message) {
+        super(message);
+    }
+
+    public EmailNotSentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmailNotSentException(Throwable cause) {
+        super(cause);
+    }
+
+    public EmailNotSentException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
