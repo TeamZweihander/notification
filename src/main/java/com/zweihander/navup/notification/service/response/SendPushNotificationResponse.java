be5053a4aa0e697375f6ae0e8efdafc3303fa8c0
georgemweshi <georgemweshi@yahoo.com>
2017-05-21 00:37:00 +0200

    added push notification response class

---
+++ src/main/java/com/zweihander/navup/notification/service/response/SendPushNotificationResponse.java
@@ -0,0 +1,11 @@
+package com.zweihander.navup.notification.service.response;
+
+import com.zweihander.navup.notification.Response;
+
+/**
+ * Created by George on 2017/05/10.
+ */
+public class SendPushNotificationResponse extends Response {
+
+
+}
